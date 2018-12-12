c5a1ab76b30feffac9ad389155f1bf7942175b09
wynstaz <377833645@qq.com>
2018-12-12 15:32:55 +0800

    代码提交

--- consumer/src/main/java/com/wynstaz/mailconsumer/rabbitmq/MailReceiver.java
+++ consumer/src/main/java/com/wynstaz/mailconsumer/rabbitmq/MailReceiver.java
@@ -9,12 +9,12 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = RabbitMQConstant.QUEUE_NAME_MAIL)
 public class MailReceiver {
 
     private static Logger LOGGER = LoggerFactory.getLogger(MailReceiver.class);
 
     @RabbitHandler
+    @RabbitListener(queues = {RabbitMQConstant.QUEUE_NAME_MAIL, RabbitMQConstant.QUEUE_NAME_MAIL})
     public void process(String context) {
         LOGGER.info("Receiver:{}", context);
     }
